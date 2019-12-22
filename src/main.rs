47e719f9d28c6bc16fec5593a1cc967b7d5a1f4e
nouman-96 <noumanqadri786@gmail.com>
2019-12-22 10:17:51 +0500

    Difference between functions and methods

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,17 @@ struct Food{
     class : String,
     season: String,
 }
+
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
 // Structure data always store on the heap and the data will be move when passed to the function
 // Whatever the data is in the structure it will be stored on the heap 
 // self, impl, print
@@ -22,6 +33,12 @@ fn main() {
     println!("{:?}",almond);
     print(&mut almond); // simple function call
     almond.impl_print(); // method function call
+    let rect1 = Rectangle { width: 30, height: 50 };
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
 }
 fn print(almond:&mut Food){
     println!("{:?}",almond);
